b852796177db2bbd7d7af275ed5a7d0c71ea6cea
Thomas Walker <thwalker@vt.edu>
2018-11-09 11:10:17 -0500

    Configure DNS with Route53

--- 01-webotron/webotron/bucket.py
+++ 01-webotron/webotron/bucket.py
@@ -28,7 +28,10 @@ class BucketManager:
         )
         self.manifest = {}
 
-
+    def get_bucket(self, bucket_name)
+        """Get a bucket by name."""
+        return self.s3.Bucket(bucket_name)
+        
     def get_region_name(self, bucket):
         """Get the bucket's region name."""
         bucket_location = \
